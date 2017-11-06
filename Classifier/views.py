a018b4852ac54871230a73e81dc2a5d935586203
dlut414 <hfy8373@gmail.com>
2017-11-06 18:18:52 +0900

    configure for deploy

--- Classifier/views.py
+++ Classifier/views.py
@@ -6,10 +6,7 @@ import os
 import sys
 dir_path = os.path.join( os.path.dirname(__file__), 'python' );
 sys.path.append( dir_path )
-from NN import NN
 
-nn = NN([3,3,3]);
-nn.load(os.path.join(dir_path, 'config'));
 # Create your views here.
 def index(request):
     CONTEXT = {
