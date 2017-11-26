596c2654ba41dabf290a59eaf2820b75e82772c0
dlut414 <hfy8373@gmail.com>
2017-11-26 22:46:12 +0900

    added a sample download link (stored in github) to pSampler

--- pMaker/views.py
+++ pMaker/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    CONTEXT = {
+        'app_name' : 'pMaker',
+    };
+    return render(request, 'pMaker/index.html', context=CONTEXT);
