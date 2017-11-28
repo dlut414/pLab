569ceed7ccb12f9cdb691dbfc8a1fb8d58b0a22c
dlut414 <hfy8373@gmail.com>
2017-11-28 19:23:51 +0900

    deleted form in pMaker

--- pMaker/views.py
+++ pMaker/views.py
@@ -2,13 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from .forms import BasicViewForm
 
 # Create your views here.
 def index(request):
     form = BasicViewForm();
     CONTEXT = {
         'app_name' : 'pMaker',
-        'form' : form,
     };
     return render(request, 'pMaker/index.html', context=CONTEXT);
