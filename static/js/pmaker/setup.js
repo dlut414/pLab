7951d3e06006a4984c42a1984c1f4ae6378d275b
dlut414 <hfy8373@gmail.com>
2017-12-01 21:30:09 +0900

    fixed bug in 'strict mode'

--- static/js/pmaker/setup.js
+++ static/js/pmaker/setup.js
@@ -1,36 +1,37 @@
+var doc = document;
 function setup(result){
-	this.header = new DataView(result, 0, 54);
-	this.width = header.getInt32(18, true);
-	this.height = header.getInt32(22, true);
-	this.rowSize = (this.width* 3 + 3) & (~3);
-	this.data = [];
-	for(var i=0;i<this.height;i++){
-		var offset = 54 + i* this.rowSize;
-		var dv = new DataView(result, offset, this.rowSize);
-		for(var j=0;j<this.width;j++){
+	doc.header = new DataView(result, 0, 54);
+	doc.width = doc.header.getInt32(18, true);
+	doc.height = doc.header.getInt32(22, true);
+	doc.rowSize = (doc.width* 3 + 3) & (~3);
+	doc.data = [];
+	for(var i=0;i<doc.height;i++){
+		var offset = 54 + i* doc.rowSize;
+		var dv = new DataView(result, offset, doc.rowSize);
+		for(var j=0;j<doc.width;j++){
 			//order: b-g-r
 			var b = dv.getUint8(3*j);
 			var g = dv.getUint8(3*j + 1);
 			var r = dv.getUint8(3*j + 2);
 			var color = ((r|0) << 16) + ((g|0) << 8) + (b|0);
-			this.data.push(color);
+			doc.data.push(color);
 		}
 	}
 }
 function make(color2type, color2vel){
 	var output = "";
 	var dp = Number($('#point_distance').val());
-	for(var i=0;i<this.height;i++){
-		for(var j=0;j<this.width;j++){
-			var id = i* this.width + j;
-			if(!color2type.has(this.data[id])) continue;
-			var type = color2type.get(this.data[id]);
+	for(var i=0;i<doc.height;i++){
+		for(var j=0;j<doc.width;j++){
+			var id = i* doc.width + j;
+			if(!color2type.has(doc.data[id])) continue;
+			var type = color2type.get(doc.data[id]);
 			var x = j* dp, y = i* dp;
 			var vx = 0, vy = 0;
-			if(!color2vel.has(this.data[id])) vx = vy = 0;
+			if(!color2vel.has(doc.data[id])) vx = vy = 0;
 			else{
-				vx = eval(color2vel.get(this.data[id])[0]);
-				vy = eval(color2vel.get(this.data[id])[1]);
+				vx = eval(color2vel.get(doc.data[id])[0]);
+				vy = eval(color2vel.get(doc.data[id])[1]);
 				vx = vx ? vx : 0;
 				vy = vy ? vy : 0;
 			}
@@ -45,14 +46,14 @@ function make(color2type, color2vel){
 }
 function download(filename, text){
 	var blob = new Blob([text], {type : 'text/plain'});
-	var element = document.createElement('a');
+	var element = doc.createElement('a');
 	// element.setAttribute('href', 'data:text/plain;charset=utf-8,' + output); //need to use percent encoding
 	element.setAttribute('href', window.URL.createObjectURL(blob));
 	element.setAttribute('download', filename);
 	element.style.display = 'none';
-	document.body.appendChild(element);
+	doc.body.appendChild(element);
 	element.click();
-	document.body.removeChild(element);
+	doc.body.removeChild(element);
 }
 $('#button-save').click(function(){
 	//color -> type
@@ -67,8 +68,8 @@ $('#button-save').click(function(){
 		alert('Length of color is different from length of type');
 		return;
 	}
-	this.color2type = new Map();
-	color.forEach( (key, i) => this.color2type.set(key, type[i]) );
+	doc.color2type = new Map();
+	color.forEach( (key, i) => doc.color2type.set(key, type[i]) );
 	//color -> vx, vy
 	var color_vel = [], string_vx = [], string_vy = [];
 	$('.color-vel').each(function(index){
@@ -80,7 +81,7 @@ $('#button-save').click(function(){
 	$('.string-vy').each(function(index){
 		string_vy.push($(this).val());
 	});
-	this.color2vel = new Map();
-	color_vel.forEach( (key, i) => this.color2vel.set(key, [string_vx[i], string_vy[i]]) );
-	make(this.color2type, this.color2vel);
+	doc.color2vel = new Map();
+	color_vel.forEach( (key, i) => doc.color2vel.set(key, [string_vx[i], string_vy[i]]) );
+	make(doc.color2type, doc.color2vel);
 });
