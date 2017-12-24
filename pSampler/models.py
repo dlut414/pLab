8d425be68252d82ead926631553c60cecb01968c
dlut414 <hfy8373@gmail.com>
2017-12-24 21:41:29 +0900

    changed diplay order in admin

--- pSampler/models.py
+++ pSampler/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class RequestInstance(models.Model):
 	class Meta:
-		ordering = ["count", "last_request"];
+		ordering = ["-count", "-last_request"];
 		
 	ip_address = models.GenericIPAddressField(protocol='both', unpack_ipv4=True, db_index=True);
 	count = models.IntegerField(default=1);
