0bd9731b891f36086d5a149284b5c8fa91dbf998
dlut414 <hfy8373@gmail.com>
2017-12-24 20:24:12 +0900

    added a model recording request ip

--- pSampler/models.py
+++ pSampler/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class RequestInstance(models.Model):
+	class Meta:
+		ordering = ["count", "last_request"];
+		
+	ip_address = models.GenericIPAddressField(protocol='both', unpack_ipv4=True, db_index=True);
+	count = models.IntegerField(default=1);
+	last_request = models.DateTimeField(auto_now=True);
+	
+	def __str__(self):
+		return self.ip_address;
--- pSampler/views.py
+++ pSampler/views.py
@@ -4,14 +4,30 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .forms import BasicViewForm
 from .forms import StreamlineForm
+from .models import RequestInstance
 
-# Create your views here.
+def get_client_ip(request):
+	xff = request.META.get('HTTP_X_FORWARDED_FOR');
+	if xff:
+		print "xff found";
+		ret = xff.split(',')[0];
+	else:
+		ret = request.META.get('REMOTE_ADDR');
+	return ret;
+
 def index(request):
-    form = BasicViewForm();
-    form_sl = StreamlineForm();
-    CONTEXT = {
-        'app_name' : 'pSampler',
-        'form' : form,
-        'form_sl' : form_sl,
-    };
-    return render(request, 'pSampler/index.html', context=CONTEXT);
+	form = BasicViewForm();
+	form_sl = StreamlineForm();
+	CONTEXT = {
+		'app_name' : 'pSampler', 
+		'form' : form, 
+		'form_sl' : form_sl,
+		};
+	ip = get_client_ip(request);
+	request_ins = RequestInstance.objects.filter(ip_address=ip);
+	if request_ins:
+		request_ins[0].count += 1;
+		request_ins[0].save();
+	else:
+		RequestInstance.objects.create(ip_address=ip);
+	return render(request, 'pSampler/index.html', context=CONTEXT);
